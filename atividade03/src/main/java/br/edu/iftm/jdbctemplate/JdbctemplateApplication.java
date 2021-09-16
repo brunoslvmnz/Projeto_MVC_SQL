61ccc7a41aee23282c980a0aee08484b96ad734f
brunoslvmnz <83134972+brunoslvmnz@users.noreply.github.com>
2021-09-15 21:31:55 -0300

    Add files via upload
    SELECT usando o thymeleaf

---
+++ atividade03/src/main/java/br/edu/iftm/jdbctemplate/JdbctemplateApplication.java
@@ -0,0 +1,39 @@
+package br.edu.iftm.jdbctemplate;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.jdbc.core.JdbcTemplate;
+
+@SpringBootApplication
+public class JdbctemplateApplication implements CommandLineRunner {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JdbctemplateApplication.class, args);
+	}
+
+	@Autowired
+	JdbcTemplate jdbcTemplate;
+
+	@Override
+	public void run(String... args) throws Exception {
+
+		jdbcTemplate.execute("DROP TABLE contatos IF EXISTS");
+		jdbcTemplate.execute("CREATE TABLE contatos(id SERIAL, nome VARCHAR(255), telefone VARCHAR(255) )");
+
+		jdbcTemplate.update("INSERT INTO contatos(nome,telefone) VALUES (?,?)", "Edson Angoti Júnior", "123");
+		jdbcTemplate.update("INSERT INTO contatos(nome,telefone) VALUES (?,?)", "José Joaquim", "123");
+		jdbcTemplate.update("INSERT INTO contatos(nome,telefone) VALUES (?,?)", "Maria Carolina", "123");
+
+		int qteContatos = this.jdbcTemplate.queryForObject("select count(*) from contatos", Integer.class);
+		String telefone = this.jdbcTemplate.queryForObject("select telefone from contatos where nome = 'Edson Angoti Júnior'", String.class);
+
+		System.out.println("Quantidade de contatos: " + qteContatos);
+		System.out.println("Telefone do Edson: " + telefone);
+
+	}
+
+}
